47e40e7e5819b1f817ba58a3452abd3f9078b187
gasada <gasada@protonmail.com>
2021-05-26 11:39:21 +0300

    feat: add working func drawing

--- src/js/index.js
+++ src/js/index.js
@@ -14,9 +14,21 @@ let lastY = 0;
 let draw = (e) => {
     if (!isDrawing) return;
     console.log(e);
+    CTX.beginPath();
+    CTX.moveTo(lastX, lastY);
+    CTX.lineTo(e.offsetX, e.offsetY);
+    CTX.stroke();
+
+    lastX = e.offsetX;
+    lastY = e.offsetY;
+
 }
 
+
 CANVAS.addEventListener('mousemove', draw);
-CANVAS.addEventListener('mousedown', () => isDrawing = true);
-CANVAS.addEventListener('mouseup', () => isDrawing = false);
+CANVAS.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+    CANVAS.addEventListener('mouseup', () => isDrawing = false);
 CANVAS.addEventListener('mouseout', () => isDrawing = false);
